a6204ce946484e493e2c2e4393e53b19d2eddf26
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-09-24 17:34:52 +0200

    AddGoogleCloudSecretManager

---
+++ src/GoogleCloudSecretManagerExtensions/GoogleCloudSecretManagerConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GoogleCloudSecretManagerExtensions;
+
+public static class GoogleCloudSecretManagerConfigurationExtensions
+{
+    public static IConfigurationBuilder AddGoogleCloudSecretManager(this IConfigurationBuilder configurationBuilder)
+    {
+        return configurationBuilder;
+    }
+}
