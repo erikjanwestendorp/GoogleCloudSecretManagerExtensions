9222d2bb154aaf174ff13ec32d7369de50dc8036
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-10-01 16:05:37 +0200

    Add CredentialsPath to appsettings

--- src/GoogleCloudSecretManagerExtensions.TestSite/AppSettings.cs
+++ src/GoogleCloudSecretManagerExtensions.TestSite/AppSettings.cs
@@ -5,5 +5,6 @@ public class AppSettings
     public class SecretManager
     {
         public string ProjectId { get; set; } = string.Empty;
+        public string CredentialsPath { get; set; } = string.Empty;
     }
 }
