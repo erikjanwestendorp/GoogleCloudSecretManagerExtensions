aa782a9231d714b7a499dc93411c861cd3dc65e6
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-10-01 15:50:02 +0200

    Add AddGoogleCloudSecretManager overload

--- src/GoogleCloudSecretManagerExtensions.TestSite/Compose/WebApplicationBuilderExtensions.cs
+++ src/GoogleCloudSecretManagerExtensions.TestSite/Compose/WebApplicationBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace GoogleCloudSecretManagerExtensions.TestSite.Compose;
+﻿using Google.Cloud.SecretManager.V1;
+
+namespace GoogleCloudSecretManagerExtensions.TestSite.Compose;
 
 public static class WebApplicationBuilderExtensions
 {
@@ -20,6 +22,16 @@ public static class WebApplicationBuilderExtensions
         {
             throw new InvalidOperationException("ProjectId is not configured in appsettings.json under 'SecretManager:ProjectId'.");
         }
+
+
+        //SecretManagerServiceClientBuilder secretClientBuilder = new SecretManagerServiceClientBuilder
+        //{
+        //    CredentialsPath = "dogwood-concept-429614-j5-f29475ed81d4.json"
+        //};
+
+        //var secretManagerServiceClient = secretClientBuilder.Build();
+
+        //builder.Configuration.AddGoogleCloudSecretManager(projectId, new SecretManagerConfigurationOptions(), secretManagerServiceClient);
 
         builder.Configuration.AddGoogleCloudSecretManager(projectId);
 
--- src/GoogleCloudSecretManagerExtensions/SecretManagerConfigurationSource.cs
+++ src/GoogleCloudSecretManagerExtensions/SecretManagerConfigurationSource.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Google.Cloud.SecretManager.V1;
 
 namespace GoogleCloudSecretManagerExtensions;
 
-public class SecretManagerConfigurationSource(string projectId, SecretManagerConfigurationOptions options) : IConfigurationSource
+public class SecretManagerConfigurationSource(string projectId, SecretManagerConfigurationOptions options, SecretManagerServiceClient secretManagerServiceClient) : IConfigurationSource
 {
     public IConfigurationProvider Build(IConfigurationBuilder builder)
     {
-        return new SecretManagerConfigurationProvider(projectId, options);
+        return new SecretManagerConfigurationProvider(projectId, options, secretManagerServiceClient);
     }
 }
